ec662bc2a6340b063d158195161250050238d22b
Lear0x <mathieu.lacroix.pro@gmail.com>
2025-01-24 15:26:33 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash en amont lancé cette commande (chmod +x setup.sh)
+#!/bin/bash
 echo "Création de l'environnement virtuel..."
-python3 -m venv venv
+python -m venv venv
 source venv/Scripts/activate
 echo "Installation des dépendances..."
 pip install -r requirements.txt
