5336ee05491dcd67e1178abb8675ef73e1d755d1
Lear0x <mathieu.lacroix.pro@gmail.com>
2025-01-24 16:02:59 +0100

    bdd

---
+++ app/database.py
@@ -0,0 +1,51 @@
+import sys
+import mysql.connector
+from config import Config
+
+def create_table():
+    connection = mysql.connector.connect(
+        host=Config.MYSQL_HOST,
+        user=Config.MYSQL_USER,
+        password=Config.MYSQL_PASSWORD,
+        database=Config.MYSQL_DB
+    )
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tweets (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            text VARCHAR(255) NOT NULL,
+            positive TINYINT(1),
+            negative TINYINT(1)
+        );
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("Table 'tweets' créée avec succès.")
+
+def fetch_tweets():
+    connection = mysql.connector.connect(
+        host=Config.MYSQL_HOST,
+        user=Config.MYSQL_USER,
+        password=Config.MYSQL_PASSWORD,
+        database=Config.MYSQL_DB
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM tweets;")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
+
+
+#lancement commande python app/database.py fetch_tweets ou python app/database.py create_table
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python database.py <create_table|fetch_tweets>")
+    elif sys.argv[1] == "create_table":
+        create_table()
+    elif sys.argv[1] == "fetch_tweets":
+        fetch_tweets()
+    else:
+        print("Action inconnue. Utilisez 'create_table' ou 'fetch_tweets'.")
--- config.py
+++ config.py
@@ -4,8 +4,9 @@ import os
 load_dotenv()
 
 class Config:
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
+    MYSQL_USER = os.getenv("MYSQL_USER", "sentiment_user")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "user_password")
+    MYSQL_DB = os.getenv("MYSQL_DB", "sentiment_db")
     FLASK_ENV = os.getenv("FLASK_ENV")
